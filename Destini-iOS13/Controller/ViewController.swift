cfe27a5b2eaa9a0f55a71fa776128fb23497794f
Dmitry <psplvd@gmail.com>
2023-02-02 13:17:20 +0100

    create project

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -6,21 +6,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left"
-    let choice2 = "Take a right"
+ var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = example[0].title
-        choice1Button.setTitle(example[0].choice1, for: .normal)
-        choice2Button.setTitle(example[0].choice2, for: .normal)
+updateUI()
 
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
-    }
-    
-}
 
+        }
+    }
+func updateUI() {
+
+}
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -3,16 +3,7 @@ import Foundation
 struct Story {
     let title: String
     let choice1: String
+    let choice1Dest: Int
     let choice2: String
-    
-//    init(title: String, choice1: String, c2: String) {
-//    //        text = t
-//    //        choice1 = c1
-//    //        choice2 = c2
-//    //    }
+    let choice2Dest: Int
 }
-let example = [
-    Story(title: "You see a fork in the road.", choice1: "Take a left", choice2: "Take a right"),
-    Story(title: "You see a tiger.", choice1: "Shout for help", choice2: "Play dead"),
-    Story(title: "You find a treasure chest.", choice1: "Open it", choice2: "Check for traps")
-]
